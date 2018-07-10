8668d0b73728ff0a2610bc6823fba4fdbb6efc10
luyang1210 <luy4@student.unimelb.edu.au>
2018-07-10 19:01:13 +1000

    Update hbos_example.py

--- examples/hbos_example.py
+++ examples/hbos_example.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Example of using HBOS for outlier detection
+"""Example of using HBOS for outlier detection (Histogram-based Outlier Score)
 """
 # Author: Yue Zhao <yuezhao@cs.toronto.edu>
 # License: BSD 2 clause
